96ee53961f19af9aa2a372b8c44fc10ebd2dd6c0
Karina Gordia <kgordya24@gmail.com>
2024-09-13 10:27:25 +0200

    adding images

---
+++ lib/resources/feeling_images.dart
@@ -0,0 +1,12 @@
+part of 'resources.dart';
+
+class FeelingImages {
+  FeelingImages._();
+
+  static const String calmness = 'assets/feeling_images/calmness.png';
+  static const String fear = 'assets/feeling_images/fear.png';
+  static const String joy = 'assets/feeling_images/joy.png';
+  static const String rage = 'assets/feeling_images/rage.png';
+  static const String sadness = 'assets/feeling_images/sadness.png';
+  static const String strength = 'assets/feeling_images/strength.png';
+}
---
+++ lib/resources/resources.dart
@@ -0,0 +1,1 @@
+part 'feeling_images.dart';
