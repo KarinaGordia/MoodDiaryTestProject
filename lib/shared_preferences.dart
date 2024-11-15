53ffa9d363b0bae4bab35d577353b75346998233
Karina Gordia <kgordya24@gmail.com>
2024-11-15 13:54:40 +0100

    removed unused dependencies

--- lib/shared_preferences.dart
+++
@@ -1,3 +0,0 @@
-//install shared preferences for saving date of the first app launch
-//https://anoop4real.medium.com/flutter-detect-a-new-day-6d18445ba6b5
-//use this solution for scroll calendar before a year of the first app launch date
